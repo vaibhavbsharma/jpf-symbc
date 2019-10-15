90f9b61dda84d9b99daac0d92af468c8779ff2d4
Vaibhav Sharma <vaibhav@umn.edu>
2019-10-15 12:35:51 -0500

    Minor printing improvements to inequality conclusion of Outputs equivalence checking

--- src/examples/veritesting/Outputs.java
+++ src/examples/veritesting/Outputs.java
@@ -22,12 +22,12 @@ public class Outputs {
         if (Outputs.class.isInstance(obj)) {
             Outputs o = (Outputs) obj;
             if (o.intOutputs.length != intOutputs.length) {
-                System.out.println("length mismatch (" + o.intOutputs.length + ", " + intOutputs.length);
+                System.out.println("length mismatch: (" + o.intOutputs.length + ", " + intOutputs.length + ")");
                 return false;
             }
             for (int i=0; i < intOutputs.length; i++) {
                 if (intOutputs[i] != o.intOutputs[i]) {
-                    System.out.println("entry " + i + " mismatch, " + intOutputs[i] + ", " + o.intOutputs[i]);
+                    System.out.println("entry " + i + " mismatch: (" + intOutputs[i] + ", " + o.intOutputs[i] + ")");
                     return false;
                 }
             }
