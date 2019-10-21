1e3ffaf2004e5ae8182450c5fc90f094abbdc69f
Vaibhav Sharma <vaibhav@umn.edu>
2019-10-21 16:16:41 -0500

    Adding scripts to run PrintTokens to Java Ranger repo

--- src/examples/veritesting/apachecli/runApacheCLI-JR_1-8sym.sh
+++ src/examples/veritesting/apachecli/runApacheCLI-JR_1-8sym.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-alias runSPF-cli='LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/export/scratch2/vaibhav/VeritestingTransformations/lib TARGET_CLASSPATH_WALA=/export/scratch2/vaibhav/VeritestingTransformations/build/apachecli/ java -Djava.library.path=/export/scratch2/vaibhav/VeritestingTransformations/lib -Xmx12288m -ea -Dfile.encoding=UTF-8 -jar /export/scratch/vaibhav/jpf-core-veritesting/build/RunJPF.jar '
+alias runSPF-cli='LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/export/scratch2/vaibhav/java-ranger/lib TARGET_CLASSPATH_WALA=/export/scratch2/vaibhav/java-ranger/build/examples/ java -Djava.library.path=/export/scratch2/vaibhav/java-ranger/lib -Xmx12288m -ea -Dfile.encoding=UTF-8 -jar /export/scratch/vaibhav/jpf-core-veritesting/build/RunJPF.jar '
 
 shopt -s expand_aliases
-VERIDIR=/export/scratch2/vaibhav/VeritestingTransformations
-TIMEOUT_MINS=1440 && export TIMEOUT_MINS
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/export/scratch2/vaibhav/VeritestingTransformations/lib && export LD_LIBRARY_PATH
-TARGET_CLASSPATH_WALA=/export/scratch2/vaibhav/VeritestingTransformations/build/apachecli/ && export TARGET_CLASSPATH_WALA
+VERIDIR=/export/scratch2/vaibhav/java-ranger
+TIMEOUT_MINS=720 && export TIMEOUT_MINS
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/export/scratch2/vaibhav/java-ranger/lib && export LD_LIBRARY_PATH
+TARGET_CLASSPATH_WALA=/export/scratch2/vaibhav/java-ranger/build/examples/ && export TARGET_CLASSPATH_WALA
 
 
 # for NSTEP in {6..7}; do
 #   for MODE in {5..2}; do
-#     echo "running ApacheCLI.$(($NSTEP))_1sym.mode$(($MODE))" && timeout $(($TIMEOUT_MINS))m  java -Djava.library.path=/export/scratch2/vaibhav/VeritestingTransformations/lib -Xmx12288m -ea -Dfile.encoding=UTF-8 -jar /export/scratch/vaibhav/jpf-core-veritesting/build/RunJPF.jar   $VERIDIR/src/apachecli/ApacheCLI.$(($NSTEP))_1sym.mode$(($MODE)).jpf >& $VERIDIR/logs/ApacheCLI.$((NSTEP))_1sym.mode$((MODE)).log 
+#     echo "running ApacheCLI.$(($NSTEP))_1sym.mode$(($MODE))" && timeout $(($TIMEOUT_MINS))m  java -Djava.library.path=/export/scratch2/vaibhav/java-ranger/lib -Xmx12288m -ea -Dfile.encoding=UTF-8 -jar /export/scratch/vaibhav/jpf-core-veritesting/build/RunJPF.jar   $VERIDIR/src/examples/veritesting/apachecli/config_files/ApacheCLI.$(($NSTEP))_1sym.mode$(($MODE)).jpf >& $VERIDIR/logs/ApacheCLI.$((NSTEP))_1sym.mode$((MODE)).log 
 #     if [ $? -eq 124 ]; then 
 #           echo "running ApacheCLI.$(($NSTEP))_1sym.mode$(($MODE)) timed out" >> $VERIDIR/logs/ApacheCLI.$((NSTEP))_1sym.mode$((MODE)).log
 #     fi
@@ -19,7 +19,7 @@ TARGET_CLASSPATH_WALA=/export/scratch2/vaibhav/VeritestingTransformations/build/
 
 for NSTEP in {8..8}; do
   for MODE in {5..5}; do
-    echo "running ApacheCLI.$(($NSTEP))_1sym.mode$(($MODE))" && timeout $(($TIMEOUT_MINS))m  java -Djava.library.path=/export/scratch2/vaibhav/VeritestingTransformations/lib -Xmx12288m -ea -Dfile.encoding=UTF-8 -jar /export/scratch/vaibhav/jpf-core-veritesting/build/RunJPF.jar   $VERIDIR/src/apachecli/ApacheCLI.$(($NSTEP))_1sym.mode$(($MODE)).jpf >& $VERIDIR/logs/ApacheCLI.$((NSTEP))_1sym.mode$((MODE)).log 
+    echo "running ApacheCLI.$(($NSTEP))_1sym.mode$(($MODE))" && timeout $(($TIMEOUT_MINS))m  java -Djava.library.path=/export/scratch2/vaibhav/java-ranger/lib -Xmx12288m -ea -Dfile.encoding=UTF-8 -jar /export/scratch/vaibhav/jpf-core-veritesting/build/RunJPF.jar   $VERIDIR/src/examples/veritesting/apachecli/config_files/ApacheCLI.$(($NSTEP))_1sym.mode$(($MODE)).jpf >& $VERIDIR/logs/ApacheCLI.$((NSTEP))_1sym.mode$((MODE)).log 
     if [ $? -eq 124 ]; then 
           echo "running ApacheCLI.$(($NSTEP))_1sym.mode$(($MODE)) timed out" >> $VERIDIR/logs/ApacheCLI.$((NSTEP))_1sym.mode$((MODE)).log
     fi
---
+++ src/examples/veritesting/printtokens2_3/runPrintTokens-SPF.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+alias runSPF-pt='LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/export/scratch2/vaibhav/java-ranger/lib TARGET_CLASSPATH_WALA=/export/scratch2/vaibhav/java-ranger/build/examples/ java -Djava.library.path=/export/scratch2/vaibhav/java-ranger/lib -Xmx12288m -ea -Dfile.encoding=UTF-8 -jar /export/scratch/vaibhav/jpf-core-veritesting/build/RunJPF.jar '
+
+shopt -s expand_aliases
+VERIDIR=/export/scratch2/vaibhav/java-ranger
+TIMEOUT_MINS=1440 && export TIMEOUT_MINS
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/export/scratch2/vaibhav/java-ranger/lib && export LD_LIBRARY_PATH
+TARGET_CLASSPATH_WALA=/export/scratch2/vaibhav/java-ranger/build/examples/ && export TARGET_CLASSPATH_WALA
+
+for NSYM in {3..6}; do # 6 symbolic inputs timeout in 24 hours
+  for MODE in {1..1}; do
+    echo "running printtokens.$(($NSYM))sym.mode$(($MODE))" && timeout $(($TIMEOUT_MINS))m  java -Djava.library.path=/export/scratch2/vaibhav/java-ranger/lib -Xmx12288m -ea -Dfile.encoding=UTF-8 -jar /export/scratch/vaibhav/jpf-core-veritesting/build/RunJPF.jar   $VERIDIR/src/examples/veritesting/printtokens2_3/printtokens.$(($NSYM))sym.mode$(($MODE)).jpf >& $VERIDIR/logs/printtokens.$((NSYM))sym.mode$((MODE)).log 
+    if [ $? -eq 124 ]; then 
+          echo "running printtokens.$(($NSYM))sym.mode$(($MODE)) timed out" >> $VERIDIR/logs/printtokens.$((NSYM))sym.mode$((MODE)).log
+    fi
+  done;
+done
