1bac2010fe7f0d4aad1d3d2e44fda89acb64d418
Vaibhav Sharma <vaibhav@umn.edu>
2019-11-17 15:23:55 -0600

    Translating GreenConstraint to ABC
    This fixes issues Java Ranger was running into with the ExSymExeResearch_true benchmark along with the issue it was running into with the CharSequenceToString benchmark

--- src/main/edu/ucsb/cs/vlab/translate/smtlib/generic/NumericConstraintTranslator.java
+++ src/main/edu/ucsb/cs/vlab/translate/smtlib/generic/NumericConstraintTranslator.java
@@ -8,13 +8,14 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import edu.ucsb.cs.vlab.translate.NormalFormTranslator;
+import edu.ucsb.cs.vlab.translate.smtlib.Results;
 import edu.ucsb.cs.vlab.translate.smtlib.TranslationManager;
-import gov.nasa.jpf.symbc.numeric.Comparator;
-import gov.nasa.jpf.symbc.numeric.Constraint;
-import gov.nasa.jpf.symbc.numeric.Expression;
-import gov.nasa.jpf.symbc.numeric.IntegerExpression;
+import gov.nasa.jpf.symbc.numeric.*;
 import gov.nasa.jpf.symbc.string.StringExpression;
 import gov.nasa.jpf.symbc.string.SymbolicCharAtInteger;
+import za.ac.sun.cs.green.expr.IntVariable;
+import za.ac.sun.cs.green.expr.Operation;
+import za.ac.sun.cs.green.expr.RealVariable;
 
 public abstract class NumericConstraintTranslator extends NormalFormTranslator<Constraint, Comparator, String> {
 	public NumericConstraintTranslator(TranslationManager manager) {
@@ -38,8 +39,64 @@ public abstract class NumericConstraintTranslator extends NormalFormTranslator<C
 	public List<String> transformChain(Constraint instance, List<String> collection) {
 		if (instance == null)
 			return collection;
-		collection.add(transform(instance));
+		if (!GreenConstraint.class.isInstance(instance))
+			collection.add(transform(instance));
+		else {
+			// TODO translate GreenConstraint to a solver string
+			GreenConstraint gC = (GreenConstraint) instance;
+			System.out.println(gC);
+			if (gC.and instanceof LinearIntegerConstraint)
+				collection.add(transform(gC.and));
+			collection.add(transformGreenExp(gC.getExp()));
+		}
 		return transformChain(instance.getTail(), collection);
+	}
+
+	private String transformGreenExp(za.ac.sun.cs.green.expr.Expression exp) {
+		if (exp instanceof Operation) {
+			Operation op = (Operation) exp;
+			String opString = getOpString(op.getOperator());
+			String ret =  "(" + opString;
+			for (int i = 0; i < op.getArity(); i++)
+				ret += " " + transformGreenExp(op.getOperand(i));
+			ret += ")" + (opString.contains("(=") ? ")" : "");
+			return ret;
+		} else {
+			if (exp instanceof IntVariable) Results.numericVariables.add(((IntVariable) exp).getName());
+			if (exp instanceof RealVariable) Results.numericVariables.add(((RealVariable) exp).getName());
+			return exp.toString();
+		}
+	}
+
+	private String getOpString(Operation.Operator operator) {
+		switch(operator) {
+			case EQ: return "=";
+			case NE: return "not (=";
+			case LT:
+			case LE:
+			case GT:
+			case GE:
+			case ADD:
+			case SUB:
+			case MUL:
+			case DIV:
+			case MOD:
+			case NEG:
+			case BIT_AND:
+			case BIT_OR:
+			case BIT_XOR:
+			case BIT_NOT:
+			case SHIFTL:
+			case SHIFTR:
+			case SHIFTUR:
+				return operator.toString();
+			case AND: return "and";
+			case OR: return "or";
+			case NOT:
+				return "not ";
+			default:
+				throw new IllegalArgumentException("unsupported translation to ABC for operator: " + operator);
+		}
 	}
 
 	static class IntermediateConstraint {
