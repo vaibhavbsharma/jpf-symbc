b3e16f707a97e004113ff2776f870555155a3a04
sohah <husse200@umn.edu>
2020-07-02 11:49:28 -0500

    adding WBS with maximum steps 5

--- src/examples/WBS.java
+++ src/examples/WBS.java
@@ -251,14 +251,20 @@ public class WBS {
 
 	}
 	
-	public static void launch(int pedal1, boolean auto1, boolean skid1, int pedal2, boolean auto2, boolean skid2, int pedal3, boolean auto3, boolean skid3) {
+	public static void launch(int pedal1, boolean auto1, boolean skid1,
+							  int pedal2, boolean auto2, boolean skid2,
+							  int pedal3, boolean auto3, boolean skid3,
+							  int pedal4, boolean auto4, boolean skid4,
+							  int pedal5, boolean auto5, boolean skid5) {
 		WBS wbs = new WBS();
 		wbs.update(pedal1, auto1, skid1);
 		wbs.update(pedal2, auto2, skid2);
-		wbs.update(pedal3, auto3, skid3);		
+		wbs.update(pedal3, auto3, skid3);
+		wbs.update(pedal4, auto4, skid4);
+		wbs.update(pedal5, auto5, skid5);
 	}
 	
 	public static void main(String[] args) {
-		launch(0,false,false,0,false,false,0,false,false);
+		launch(0,false,false,0,false,false,0,false,false, 0, false, false, 0, false, false);
 	}
 }
