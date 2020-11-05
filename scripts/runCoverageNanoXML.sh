507a546061638d4b0d858a01bbf4fe2992232bd5
sohah <husse200@umn.edu>
2020-11-05 14:09:35 -0600

    changing the folder name of nanoxml

--- scripts/runCoverageNanoXML.sh
+++ scripts/runCoverageNanoXML.sh
@@ -29,10 +29,10 @@ MAX_STEPS=$2 && export MAX_STEPS
 echo "maxsteps is $MAX_STEPS"
 
 
-runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/NanoXML/NanoXMLCollect.jpf >& $COVERAGEDIR/logs/NanoXML/NanoXMLCollect_steps$2.log
-#runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/NanoXML/NanoXMLCollect_Prune.jpf >& $COVERAGEDIR/logs/NanoXML/NanoXMLCollect_Prune_steps$2.log
-runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/NanoXML/NanoXMLCollect_Guide.jpf >& $COVERAGEDIR/logs/NanoXML/NanoXMLCollect_Guide_steps$2.log
-#runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/NanoXML/NanoXMLCollect_Prune_Guide.jpf >& $COVERAGEDIR/logs/NanoXML/NanoXMLCollect_Prune_Guide_steps$2.log
+runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/nanoxml/NanoXMLCollect.jpf >& $COVERAGEDIR/logs/nanoxml/NanoXMLCollect_steps$2.log
+#runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/nanoxml/NanoXMLCollect_Prune.jpf >& $COVERAGEDIR/logs/nanoxml/NanoXMLCollect_Prune_steps$2.log
+runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/nanoxml/NanoXMLCollect_Guide.jpf >& $COVERAGEDIR/logs/nanoxml/NanoXMLCollect_Guide_steps$2.log
+#runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/nanoxml/NanoXMLCollect_Prune_Guide.jpf >& $COVERAGEDIR/logs/nanoxml/NanoXMLCollect_Prune_Guide_steps$2.log
 
-runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/NanoXML/NanoXMLJR_Collect.jpf >& $COVERAGEDIR/logs/NanoXML/NanoXMLJRCollect_steps$2.log
-runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/NanoXML/NanoXMLJR_Collect_Guide.jpf >& $COVERAGEDIR/logs/NanoXML/NanoXMLJR_Collect_Guide_steps$2.log
+runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/nanoxml/NanoXMLJR_Collect.jpf >& $COVERAGEDIR/logs/nanoxml/NanoXMLJRCollect_steps$2.log
+runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/nanoxml/NanoXMLJR_Collect_Guide.jpf >& $COVERAGEDIR/logs/nanoxml/NanoXMLJR_Collect_Guide_steps$2.log
