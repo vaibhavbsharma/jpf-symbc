07a824cadfe533ad4bb381ae6979e16ee2cce8b6
sohah <husse200@umn.edu>
2020-11-18 12:16:01 -0600

    fixing the lcmp and lshl bytecodes to maintain the stack appropriatly and to do a bitwise anding before shifting

--- src/main/gov/nasa/jpf/symbc/bytecode/LSHL.java
+++ src/main/gov/nasa/jpf/symbc/bytecode/LSHL.java
@@ -17,10 +17,7 @@
  */
 package gov.nasa.jpf.symbc.bytecode;
 
-import gov.nasa.jpf.symbc.numeric.BinaryLinearIntegerExpression;
-import gov.nasa.jpf.symbc.numeric.IntegerConstant;
-import gov.nasa.jpf.symbc.numeric.IntegerExpression;
-import gov.nasa.jpf.symbc.numeric.Operator;
+import gov.nasa.jpf.symbc.numeric.*;
 import gov.nasa.jpf.vm.Instruction;
 import gov.nasa.jpf.vm.StackFrame;
 import gov.nasa.jpf.vm.ThreadInfo;
@@ -45,7 +42,7 @@ public class LSHL extends gov.nasa.jpf.jvm.bytecode.LSHL {
 	    	long v2 = sf.popLong();
 	    	sf.pushLong(v2 << v1);
 
-	    	IntegerExpression result = null;
+	    	Expression result = null;
 	    	if (sym_v1 != null) {
 	    		if (sym_v2 != null) {
 					//result = sym_v1._shiftL(sym_v2);
@@ -56,11 +53,16 @@ public class LSHL extends gov.nasa.jpf.jvm.bytecode.LSHL {
 					//result = sym_v1._shiftL(v2);
 					BinaryLinearIntegerExpression sym_shift_val = new BinaryLinearIntegerExpression((IntegerExpression) sym_v1, Operator.AND, new IntegerConstant(63));
 
-					result = (new IntegerConstant((int) v2))._shiftL(sym_shift_val);
+					result = new IntegerConstant(v2)._shiftL(sym_shift_val);
+
+//					BinaryRealExpression sym_shift_val = new BinaryRealExpression(new )
+//					result = new RealConstant(v2)._shiftL(sym_shift_val);
+
+
 				}
 	    	}
 	    	else if (sym_v2 != null) {
-	    		result = sym_v2._shiftL(v1);
+				result = new IntegerConstant(v2)._shiftL(v1 & 63);
 	    	}
 
 	    	sf.setLongOperandAttr(result);
