d1f3fb9742383e5fb44df686432a7f08e5e34dcb
soha <soha@cs-soha-u.cs.umn.edu>
2021-11-11 18:17:26 -0600

    using shift only when we are running 64 bit vector solving

--- src/main/gov/nasa/jpf/symbc/numeric/GreenPbTranslator.java
+++ src/main/gov/nasa/jpf/symbc/numeric/GreenPbTranslator.java
@@ -163,7 +163,7 @@ class GreenPbTranslator extends Visitor {
                     Expr addVal = (Expr) context.plus(l, r);
                     String[] dp = SymbolicInstructionFactory.dp;
 
-                    if (dp[0].equalsIgnoreCase("z3bitvector") || dp[0].equalsIgnoreCase("z3bitvectorinc")) {
+                    if ((dp[0].equalsIgnoreCase("z3bitvector") || dp[0].equalsIgnoreCase("z3bitvectorinc")) && SymbolicInstructionFactory.bvlength == 64) {
                         if (allIntOperands(operation)) {
                             Expr shiftLVal = (Expr) context.shiftL(addVal, 32);
                             Expr shiftRLVal = (Expr) context.shiftR(shiftLVal, 32);
@@ -174,7 +174,7 @@ class GreenPbTranslator extends Visitor {
                 case SUB:
                     Expr subVal = (Expr) context.minus(l, r);
                     dp = SymbolicInstructionFactory.dp;
-                    if (dp[0].equalsIgnoreCase("z3bitvector") || dp[0].equalsIgnoreCase("z3bitvectorinc")) {
+                    if ((dp[0].equalsIgnoreCase("z3bitvector") || dp[0].equalsIgnoreCase("z3bitvectorinc")) && SymbolicInstructionFactory.bvlength == 64) {
                         if (allIntOperands(operation)) {
                             Expr shiftLVal = (Expr) context.shiftL(subVal, 32);
                             Expr shiftRLVal = (Expr) context.shiftR(shiftLVal, 32);
@@ -185,7 +185,7 @@ class GreenPbTranslator extends Visitor {
                 case MUL:
                     Expr mulVal = (Expr) context.mult(l, r);
                     dp = SymbolicInstructionFactory.dp;
-                    if (dp[0].equalsIgnoreCase("z3bitvector") || dp[0].equalsIgnoreCase("z3bitvectorinc")) {
+                    if ((dp[0].equalsIgnoreCase("z3bitvector") || dp[0].equalsIgnoreCase("z3bitvectorinc")) && SymbolicInstructionFactory.bvlength == 64) {
                         if (allIntOperands(operation)) {
                             Expr shiftLVal = (Expr) context.shiftL(mulVal, 32);
                             Expr shiftRLVal = (Expr) context.shiftR(shiftLVal, 32);
