2078cb9284e98d0b6b1c1d9838d105c14176ff9e
soha <soha@cs-soha-u.cs.umn.edu>
2022-02-02 08:03:50 -0600

    adding serialization to NanoXML

--- scripts/partialTCGProblems/runCoverageNanoXML.sh
+++ scripts/partialTCGProblems/runCoverageNanoXML.sh
@@ -25,7 +25,7 @@ runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/nanoxml/partialpro
 && echo "SPF path finished" \
 && runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/nanoxml/partialproblem/NanoXMLJR_Collect.mode2.jpf >& $COVERAGEDIR/logs/log_dumpxml/partialproblem/nanoXMLJRCollect.mode2_steps$MAX_STEPS.log \
 && echo "JR mode2 finished" \
-&& runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/nanoxml/partialproblem/NanoXMLJR_Collect.mode3.jpf >& $COVERAGEDIR/logs/log_dumpxml/partialproblem/nanoXMLJRCollect.mode3_steps$MAX_STEPS.log \
+&&runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/nanoxml/partialproblem/NanoXMLJR_Collect.mode3.jpf >& $COVERAGEDIR/logs/log_dumpxml/partialproblem/nanoXMLJRCollect.mode3_steps$MAX_STEPS.log \
 && echo "JR mode3 finished" \
 && runCoverage $COVERAGEDIR/src/examples/tcgbenchmarks/runconfig/nanoxml/partialproblem/NanoXMLJR_Collect.mode4.jpf >& $COVERAGEDIR/logs/log_dumpxml/partialproblem/nanoXMLJRCollect.mode4_steps$MAX_STEPS.log \
 && echo "JR mode4 finished" \
